4281ba9fb90fdf4a18946a0eb143c65d38c792d8
Matt <147024601+matthew-simms-298@users.noreply.github.com>
2023-10-30 23:40:42 -0600

    Did some work on the database updating. formData is undefined though

--- assignment-2/src/app/configuration/page.js
+++ assignment-2/src/app/configuration/page.js
@@ -1,48 +1,166 @@
+'use client'
 import StandardFooter from "@/components/StandardFooter";
+import React, {useState} from 'react'
+
 export default function Configuration() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between bg-purple-200 text-black">
-            <div class="w-screen h-12 border-b-black-2 text-center flex justify-evenly">
-                <div class="w-1/2 border-r-black-2 pr-5 bg-purple-800 text-white pt-3 text-4x1">
-                    <a href="/" className="text-center">Homepage</a>
-                </div>
-                <div class="w-1/2 border-r-black-2 pr-5 text-gray-700 bg-purple-200 pt-3 text-3x1">
-                    <p class="text-center font-bold">Students</p>
-                </div>
-            </div>
-            <div className="bg-gray-100 p-10 m-10 rounded-lg shadow-lg">
-                <h1 className="text-4xl font-bold text-center p-1 mb-15">Add Student Information</h1>
-                <form>
-                    <div className="flex flex-col items-center justify-center">
-                        <label htmlFor="FirstName">First Name:</label>
-                        <input type="text" id="FirstName" name="FirstName" placeholder="Example: Marcus"className="border border-gray-500 rounded-md p-1 m-1 hover:bg-purple-200"/>
-                        <label htmlFor="LastName">Last Name:</label>
-                        <input type="text" id="LastName" name="LastName" placeholder="Example: DeMaximus" className="border border-gray-500 rounded-md p-1 m-1 hover:bg-purple-200"/>
-                        <p>Birthdate:</p>
-                        <p className="text-xs italic text-grey-300">Year, Month, Day</p>
-                        <div className="">
-                            {/*
+  async function loadData() {
+    const response = await fetch("http://localhost:5000/students")
+      .then((response) => response.json())
+      .then((data) => setStudent(data));
+  }
+
+  const setFormData = () => {
+    const [formData, setFormData] = useState({
+      firstName: "",
+      lastName: "",
+      birthYear: "",
+      brithMonth: "",
+      brithDay: "",
+      grade: "",
+    });
+  };
+
+  const [message, setMessage] = useState("");
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://Localhost:5000/students", {
+        method: "POST",
+        hearders: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        setMessage("Student registered successfully!");
+        setFormData({
+          firstName: "",
+          lastName: "",
+          birthYear: "",
+          brithMonth: "",
+          brithDay: "",
+          grade: "",
+        });
+      } else {
+        setMessage("INVALID INPUT");
+      }
+    } catch (error) {
+      setMessage("ERROR");
+    }
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between bg-purple-200 text-black">
+      <div class="w-screen h-12 border-b-black-2 text-center flex justify-evenly">
+        <div class="w-1/2 border-r-black-2 pr-5 bg-purple-800 text-white pt-3 text-4x1">
+          <a href="/" className="text-center">
+            Homepage
+          </a>
+        </div>
+        <div class="w-1/2 border-r-black-2 pr-5 text-gray-700 bg-purple-200 pt-3 text-3x1">
+          <p class="text-center font-bold">Students</p>
+        </div>
+      </div>
+      <div className="bg-gray-100 p-10 m-10 rounded-lg shadow-lg">
+        <h1 className="text-4xl font-bold text-center p-1 mb-15">
+          Add Student Information
+        </h1>
+        <form onSubmit={handleSubmit()}>
+          <div className="flex flex-col items-center justify-center">
+            <label htmlFor="FirstName">First Name:</label>
+            <input
+              type="text"
+              id="FirstName"
+              name="FirstName"
+              value={formData.firstName}
+              placeholder="Example: Marcus"
+              className="border border-gray-500 rounded-md p-1 m-1 hover:bg-purple-200"
+              required
+            />
+            <label htmlFor="LastName">Last Name:</label>
+            <input
+              type="text"
+              id="LastName"
+              name="LastName"
+              value={formData.lastName}
+              onChange={handleInputChange()}
+              placeholder="Example: DeMaximus"
+              className="border border-gray-500 rounded-md p-1 m-1 hover:bg-purple-200"
+              required
+            />
+            <p>Birthdate:</p>
+            <p className="text-xs italic text-grey-300">Year, Month, Day</p>
+            <div className="">
+              {/*
                             <div>
                                 <label htmlFor="birthyear">Year: </label>
                                 <label htmlFor="birthmonth">Month: </label>
                                 <label htmlFor="birthday">Day: </label>
                             </div>
                             */}
-                            <div>
-                                <input type="text" id="birthyear" name="birthyear" placeholder="2023" className="border border-gray-500 rounded-md p-1 m-1 w-12 hover:bg-purple-200"/>
-                                <input type="text" id="birthday" name="birthday" placeholder="10" className="border border-gray-500 rounded-md p-1 m-1 w-7 hover:bg-purple-200"/>
-                                <input type="text" id="birthmonth" name="birthmonth" placeholder="12" className="border border-gray-500 rounded-md p-1 m-1 w-7 hover:bg-purple-200"/>
-                                
-                            </div>
-                        </div>
-                        
-                        <label htmlFor="grade">Grade: </label>
-                        <input type="text" id="grade" name="grade" placeholder="12" className="border border-gray-500 rounded-md p-1 m-1 hover:bg-purple-200"/>                        
-                        <button type="submit" className="border border-gray-500 rounded-md p-1 m-1 hover:bg-purple-200">Save</button>
-                    </div>
-                </form>
+              <div>
+                <input
+                  type="text"
+                  id="birthyear"
+                  name="birthyear"
+                  onChange={handleInputChange()}
+                  placeholder="2023"
+                  value={formData.birthYear}
+                  className="border border-gray-500 rounded-md p-1 m-1 w-12 hover:bg-purple-200"
+                  required
+                />
+                <input
+                  type="text"
+                  id="birthday"
+                  name="birthday"
+                  placeholder="10"
+                  onChange={handleInputChange()}
+                  value={formData.brithDaye}
+                  className="border border-gray-500 rounded-md p-1 m-1 w-7 hover:bg-purple-200"
+                  required
+                />
+                <input
+                  type="text"
+                  id="birthmonth"
+                  name="birthmonth"
+                  onChange={handleInputChange()}
+                  placeholder="12"
+                  value={formData.brithMonth}
+                  className="border border-gray-500 rounded-md p-1 m-1 w-7 hover:bg-purple-200"
+                  required
+                />
+              </div>
             </div>
-            <StandardFooter/>
-        </main>
-    );
+
+            <label htmlFor="grade">Grade: </label>
+            <input
+              type="text"
+              id="grade"
+              name="grade"
+              placeholder="12"
+              onChange={handleInputChange()}
+              value={formData.grade}
+              className="border border-gray-500 rounded-md p-1 m-1 hover:bg-purple-200"
+              required
+            />
+            <button
+              type="submit"
+              className="border border-gray-500 rounded-md p-1 m-1 hover:bg-purple-200"
+            >
+              Save
+            </button>
+          </div>
+        </form>
+      </div>
+      <StandardFooter />
+    </main>
+  );
 }
--- assignment-2/src/components/Student.jsx
+++ assignment-2/src/components/Student.jsx
@@ -1,11 +1,10 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-
+"use client";
+import React, { useEffect, useState } from "react";
 
 const Student = () => {
-    const [students, setStudent] = React.useState([])
+  const [students, setStudent] = React.useState([]);
 
-     /*const [studentNames, setStudentNames] = useState(['']);
+  /*const [studentNames, setStudentNames] = useState(['']);
     const [studentBirthdates, setStudentBirthdates] = useState(['']);
     const [studentGrades, setStudentGrades] = useState(['']);
 
@@ -21,14 +20,8 @@ const Student = () => {
         setStudentGrades[studentNames,targetItem];
     }; */
 
-    useEffect(() => {
-      fetch("http://localhost:5000/students")
-      .then(response => response.json())
-      .then(data => setStudent(data))
-    })
-
-    return (
-        /*<div className="bg-blue-200 rounded-md pd-5 mb-5">
+  return (
+    /*<div className="bg-blue-200 rounded-md pd-5 mb-5">
           <p className="underline font-bold">Student Names</p>
           <ul className="list-disc ml-4">
             {releaseYears.map((item,index) => (
@@ -37,18 +30,21 @@ const Student = () => {
           </ul>
         </div> */
 
-        <div className="flex flex-col items-center justify-center">
-          {/*This prints a drop down of all the students*/}
-          <select className='flex flex-row items-center justify-center rounded-md'>
-            <option value="name">student.first + student.last</option>
-          
-          </select>
-          <div className="flex flex-col items-center justify-center m-2">
-              <p className='border-y border-slate-300 px-2'>ID FName LName Birthday Grade</p>
-              <p className='border-y border-slate-300 px-2'>ID FName LName Birthday Grade</p>
-          </div>
+    <div className="flex flex-col items-center justify-center">
+      {/*This prints a drop down of all the students*/}
+      <select className="flex flex-row items-center justify-center rounded-md">
+        <option value="name">student.first + student.last</option>
+      </select>
+      <div className="flex flex-col items-center justify-center m-2">
+        <p className="border-y border-slate-300 px-2">
+          ID FName LName Birthday Grade
+        </p>
+        <p className="border-y border-slate-300 px-2">
+          ID FName LName Birthday Grade
+        </p>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Student;
