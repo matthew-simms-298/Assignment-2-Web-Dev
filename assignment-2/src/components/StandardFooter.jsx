e8f9f7147dc10798e4e5b76a1d6e9de84a062a8f
TheodoreWells <119262851+TheodoreWells@users.noreply.github.com>
2023-11-06 15:57:48 -0700

    Added logo

--- assignment-2/src/components/StandardFooter.jsx
+++ assignment-2/src/components/StandardFooter.jsx
@@ -1,7 +1,16 @@
 import react from 'react';
+import Image from 'next/image';
 export default function StandardFooter() {
-    return (
+  const imgSizeFactor = 4;  
+  
+  return (
         <footer className="bg-purple-800 p-2 w-full pt-4 flex items-center justify-center border-b-yellow-100">
+        <Image
+          src="/images/newicon.png"
+          alt='School logo depicting school name on top of a square pattern of purple encapsulated by a gradiented gold circle'
+          width={520/imgSizeFactor}
+          height={520/imgSizeFactor}
+        />
         <div className="flex flex-row text-md text-white">
           <div className="p-5">
             <p className="font-bold text-2xl">New Generation Highschool</p>
